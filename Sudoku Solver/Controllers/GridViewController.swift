1ca6ec5ff8a81f89e2b2460ffa4286938bef0fbe
rahul228646 <rahulkaushik228646@gmail.com>
2022-10-27 00:02:34 +0530

    UI Updated

--- Sudoku Solver/Controllers/GridViewController.swift
+++ Sudoku Solver/Controllers/GridViewController.swift
@@ -1,44 +1,45 @@
 //
-//  ResultViewController.swift
+//  ViewController.swift
 //  Sudoku Solver
 //
-//  Created by rahul kaushik on 26/10/22.
+//  Created by rahul kaushik on 25/10/22.
 //
 
 import UIKit
 
-class ResultViewController: UIViewController {
+class GridViewController: UIViewController {
     
-    private var board = [[Character]]()
+    let sudokuModel = SudokuModel()
     
     private let titleLabel : UILabel = {
         let label = UILabel()
-        label.text = "Result"
+        label.text = "Sudoku Solver"
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = .systemFont(ofSize: 32, weight: .heavy)
         label.numberOfLines = 0
         label.textAlignment = .center
-        
+
         return label
     }()
     
     private let collectionView : UICollectionView = {
         let layout = UICollectionViewFlowLayout()
-        layout.minimumLineSpacing = 10
-        layout.minimumInteritemSpacing = 10
+        
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        
-        collectionView.register(ResultCollectionViewCell.self , forCellWithReuseIdentifier: ResultCollectionViewCell.identifier)
-        
+        collectionView.register(GridCollectionViewCell.self , forCellWithReuseIdentifier: GridCollectionViewCell.identifier)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.showsHorizontalScrollIndicator = false
+        collectionView.layer.borderWidth = 2
+        collectionView.layer.borderColor = UIColor.label.cgColor
         return collectionView
         
     }()
     
-    private let ReCalulateButton : UIButton = {
+    private let solveButton : UIButton = {
         let button = UIButton()
-        button.setTitle("Recalulate", for: .normal)
+        button.setTitle("Solve", for: .normal)
         button.layer.cornerRadius = 5
         button.backgroundColor = .systemBlue
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -46,27 +47,28 @@ class ResultViewController: UIViewController {
         return button
     }()
     
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemBackground
         view.addSubview(titleLabel)
         view.addSubview(collectionView)
-        view.addSubview(ReCalulateButton)
+        view.addSubview(solveButton)
         collectionView.frame = view.bounds
         collectionView.delegate = self
         collectionView.dataSource = self
-        ReCalulateButton.addTarget(self, action: #selector(recalculateAction), for: .touchUpInside)
+        solveButton.addTarget(self, action: #selector(solveAction), for: .touchUpInside)
         configLayout()
     }
     
-    func configResult(board : [[Character]]) {
-        self.board = board
-    }
-    
-    @objc func recalculateAction() {
-        navigationController?.popViewController(animated: true)
+    @objc func solveAction() {
+        sudokuModel.solve()
+        let board = sudokuModel.getBoard()
+        let nextViewController = ResultViewController()
+        nextViewController.configResult(board: board)
+        navigationController?.pushViewController(nextViewController, animated: true)
+        
     }
     
     func configLayout() {
@@ -81,10 +83,10 @@ class ResultViewController: UIViewController {
             titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             titleLabel.bottomAnchor.constraint(equalTo: collectionView.topAnchor, constant: -20),
             
-            ReCalulateButton.heightAnchor.constraint(equalToConstant: 50),
-            ReCalulateButton.topAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 30),
-            ReCalulateButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: view.frame.width*0.05),
-            ReCalulateButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -(view.frame.width*0.05)),
+            solveButton.heightAnchor.constraint(equalToConstant: 50),
+            solveButton.topAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 30),
+            solveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: view.frame.width*0.05),
+            solveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -(view.frame.width*0.05)),
             
         ])
     }
@@ -92,25 +94,42 @@ class ResultViewController: UIViewController {
 
 }
 
-extension ResultViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+extension GridViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 81
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: ((view.frame.width*0.9)-80)/9, height: ((view.frame.height*0.6)-80)/9)
+        return CGSize(width: ((view.frame.width*0.9))/9, height: ((view.frame.height*0.6))/9)
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ResultCollectionViewCell.identifier, for: indexPath) as? ResultCollectionViewCell else {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GridCollectionViewCell.identifier, for: indexPath) as? GridCollectionViewCell else {
             return UICollectionViewCell()
             
         }
-        cell.config(val: String(board[indexPath.row/9][indexPath.row%9]))
+        cell.index(position: indexPath.row)
+        cell.delegate = self
         cell.layer.borderColor = UIColor.label.cgColor
-        cell.layer.borderWidth = 2
+        cell.layer.borderWidth = 0.5
         
         return cell
     }
     
     
 }
+
+extension GridViewController : GridCollectionViewCellDelegate {
+    
+    func collectionViewTableViewCellDidTapCell(_ cell: GridCollectionViewCell, input: String?, pos : Int) {
+      
+        var val = input ?? "."
+        if(val == "") {
+            val = "."
+        }
+        sudokuModel.editBoard(row: pos/9, col: pos%9, val: Character(val))
+    }
+    
+    
+}
+
+
--- Sudoku Solver/Controllers/ResultViewController.swift
+++ Sudoku Solver/Controllers/ResultViewController.swift
@@ -1,42 +1,47 @@
 //
-//  ViewController.swift
+//  ResultViewController.swift
 //  Sudoku Solver
 //
-//  Created by rahul kaushik on 25/10/22.
+//  Created by rahul kaushik on 26/10/22.
 //
 
 import UIKit
 
-class GridViewController: UIViewController {
+class ResultViewController: UIViewController {
     
-    let sudokuModel = SudokuModel()
+    private var board = [[Character]]()
     
     private let titleLabel : UILabel = {
         let label = UILabel()
-        label.text = "Sudoku Solver"
+        label.text = "Result"
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = .systemFont(ofSize: 32, weight: .heavy)
         label.numberOfLines = 0
         label.textAlignment = .center
-
+        
         return label
     }()
     
     private let collectionView : UICollectionView = {
         let layout = UICollectionViewFlowLayout()
-        layout.minimumLineSpacing = 10
-        layout.minimumInteritemSpacing = 10
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.register(GridCollectionViewCell.self , forCellWithReuseIdentifier: GridCollectionViewCell.identifier)
+        
+        collectionView.register(ResultCollectionViewCell.self , forCellWithReuseIdentifier: ResultCollectionViewCell.identifier)
+        
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.showsHorizontalScrollIndicator = false
+        
+        collectionView.layer.borderWidth = 2
+        collectionView.layer.borderColor = UIColor.label.cgColor
         return collectionView
         
     }()
     
-    private let solveButton : UIButton = {
+    private let ReCalulateButton : UIButton = {
         let button = UIButton()
-        button.setTitle("Solve", for: .normal)
+        button.setTitle("Recalulate", for: .normal)
         button.layer.cornerRadius = 5
         button.backgroundColor = .systemBlue
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -44,28 +49,27 @@ class GridViewController: UIViewController {
         return button
     }()
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemBackground
         view.addSubview(titleLabel)
         view.addSubview(collectionView)
-        view.addSubview(solveButton)
+        view.addSubview(ReCalulateButton)
         collectionView.frame = view.bounds
         collectionView.delegate = self
         collectionView.dataSource = self
-        solveButton.addTarget(self, action: #selector(solveAction), for: .touchUpInside)
+        ReCalulateButton.addTarget(self, action: #selector(recalculateAction), for: .touchUpInside)
         configLayout()
     }
     
-    @objc func solveAction() {
-        sudokuModel.solve()
-        let board = sudokuModel.getBoard()
-        let nextViewController = ResultViewController()
-        nextViewController.configResult(board: board)
-        navigationController?.pushViewController(nextViewController, animated: true)
-        
+    func configResult(board : [[Character]]) {
+        self.board = board
+    }
+    
+    @objc func recalculateAction() {
+        navigationController?.popViewController(animated: true)
     }
     
     func configLayout() {
@@ -80,10 +84,10 @@ class GridViewController: UIViewController {
             titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             titleLabel.bottomAnchor.constraint(equalTo: collectionView.topAnchor, constant: -20),
             
-            solveButton.heightAnchor.constraint(equalToConstant: 50),
-            solveButton.topAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 30),
-            solveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: view.frame.width*0.05),
-            solveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -(view.frame.width*0.05)),
+            ReCalulateButton.heightAnchor.constraint(equalToConstant: 50),
+            ReCalulateButton.topAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 30),
+            ReCalulateButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: view.frame.width*0.05),
+            ReCalulateButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -(view.frame.width*0.05)),
             
         ])
     }
@@ -91,42 +95,25 @@ class GridViewController: UIViewController {
 
 }
 
-extension GridViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+extension ResultViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 81
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: ((view.frame.width*0.9)-80)/9, height: ((view.frame.height*0.6)-80)/9)
+        return CGSize(width: ((view.frame.width*0.9))/9, height: ((view.frame.height*0.6))/9)
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GridCollectionViewCell.identifier, for: indexPath) as? GridCollectionViewCell else {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ResultCollectionViewCell.identifier, for: indexPath) as? ResultCollectionViewCell else {
             return UICollectionViewCell()
             
         }
-        cell.index(position: indexPath.row)
-        cell.delegate = self
+        cell.config(val: String(board[indexPath.row/9][indexPath.row%9]))
         cell.layer.borderColor = UIColor.label.cgColor
-        cell.layer.borderWidth = 2
+        cell.layer.borderWidth = 0.5
         
         return cell
     }
     
     
 }
-
-extension GridViewController : GridCollectionViewCellDelegate {
-    
-    func collectionViewTableViewCellDidTapCell(_ cell: GridCollectionViewCell, input: String?, pos : Int) {
-      
-        var val = input ?? "."
-        if(val == "") {
-            val = "."
-        }
-        sudokuModel.editBoard(row: pos/9, col: pos%9, val: Character(val))
-    }
-    
-    
-}
-
-
